5c59db5cf11e3f3b0cc20d2a88c16a202a4fd919
abaysankar <abaysankarbmdigitalutilisation@gmail.com>
2024-07-24 10:36:34 +0530

    added api for create order and payment page dynammic

--- src/Component/Payment/payment.jsx
+++ src/Component/Payment/payment.jsx
@@ -8,41 +8,38 @@ import { Link, useNavigate, useParams } from "react-router-dom";
 import { CiSquareMinus, CiSquarePlus } from "react-icons/ci";
 import { faLessThanEqual } from "@fortawesome/free-solid-svg-icons/faLessThanEqual";
 import ChangeAddress from "../OrderSummary/ChangeAddress";
+import { createOrder } from "../../action/createOrderAction";
 
 function Payment() {
-
   const [selectedMethod, setSelectedMethod] = useState('');
-
   const [captcha, setCaptcha] = useState('');
   const [input, setInput] = useState('');
-  const navigate = useNavigate()
-  const {id}= useParams()
-  const dispatch = useDispatch()
-  const userDetails = useSelector(state=>state?.getUser?.user)
-  const user = checkUser()
-  const [selectedAddress, setSelectedAddress ]= useState({ })
-  const [modal , setModal] = useState(false);
-  const [addressModal , setAddressModal] = useState(false)
-  const [quantity ,setQuantity] = useState(1)
-  const [updating, setUpdating] = useState(false)
-  const orderDetails  = useSelector(state => state.OrderSummary?.data)
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  const userDetails = useSelector(state => state?.getUser?.user);
+  const user = checkUser();
+  const [selectedAddress, setSelectedAddress] = useState({});
+  const [modal, setModal] = useState(false);
+  const [addressModal, setAddressModal] = useState(false);
+  const [quantity, setQuantity] = useState(1);
+  const [updating, setUpdating] = useState(false);
+  const orderDetails = useSelector(state => state.OrderSummary?.data);
+  const [reqProducts, setReqProducts] = useState({});
 
-  useEffect(()=>{
-      dispatch(getProductDetails(id))
-     
-  },[dispatch])
+  useEffect(() => {
+    dispatch(getProductDetails(id));
+  }, [dispatch, id]);
 
- useEffect(()=>{
-  if(userDetails){
-    setSelectedAddress(userDetails.shipping_address[0])
-  }
- },[userDetails])
+  useEffect(() => {
+    if (userDetails) {
+      setSelectedAddress(userDetails.shipping_address[0]);
+    }
+  }, [userDetails]);
 
-  const handleAddress = useCallback(()=>{
-      setAddressModal(true)
-
-  },[])
-
+  const handleAddress = useCallback(() => {
+    setAddressModal(true);
+  }, []);
 
   const generateCaptcha = () => {
     const randomCaptcha = Math.random().toString().substring(2, 6);
@@ -53,16 +50,6 @@ function Payment() {
     setInput(e.target.value);
   };
 
-  const handleSubmit = (e) => {
-    // e.preventDefault();
-    if (input === captcha) {
-      alert('CAPTCHA verified successfully');
-    } else {
-      alert('Incorrect CAPTCHA');
-    }
-  };
-
-  // Generate CAPTCHA when the component mounts
   useEffect(() => {
     generateCaptcha();
   }, []);
@@ -71,10 +58,26 @@ function Payment() {
     setSelectedMethod(e.target.value);
   };
 
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   alert(`Selected Payment Method: ${selectedMethod}`);
-  // };
+  const handleReqBody = () => {
+    let products = [];
+    orderDetails.orderItems.forEach(product => {
+      let obj = {};
+      obj.quantity = product.quantity;
+      obj.product = product.product._id; // Fixed key
+      obj.total_price = product.mrp_price; // Fixed key
+      obj.payable_price = product.selling_price; // Fixed key
+      obj.discount = product.discounting_price; // Fixed key
+      products.push(obj);
+    });
+    setReqProducts({ ...reqProducts, products, shippingAddress: selectedAddress.billing_address, billingAddress: selectedAddress.billing_address, shippingMethod: 'Standard Shipping' });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    handleReqBody();
+    await dispatch(createOrder(reqProducts));
+  };
+
   return (
     <section className={css.maincontainer}>
       <section className={css.leftcontainer}>
@@ -86,166 +89,175 @@ function Payment() {
             <div className={`${css.textcontent}`}>
               <h5 className="">LOGIN</h5>
               <p>
-                <span className="flex-nowrap">{userDetails && userDetails.name }</span>{userDetails && userDetails.mobile}
+                <span className="flex-nowrap">{userDetails && userDetails.name}</span>{userDetails && userDetails.mobile}
               </p>
             </div>
           </main>
           <div className={css.buttonContent}>
-            <button onClick={()=>setModal(true)}>change</button>
+            <button onClick={() => setModal(true)}>change</button>
           </div>
         </div>
-       {selectedAddress ?( 
-        <div className={css.box}>
-          <main>
-            <div>
-              <p className="text-primary">2</p>
+        {selectedAddress ? (
+          <div className={css.box}>
+            <main>
+              <div>
+                <p className="text-primary">2</p>
+              </div>
+              <div className={`${css.addresscontent}`}>
+                <h5 className="">DELIVERY ADDRESS</h5>
+                <p className={css.address}>
+                  <span>{selectedAddress.fullName}</span> -<span>{selectedAddress.mobile}</span> ,{selectedAddress.billing_address} <span>pinCode:{selectedAddress.pinCode}</span>
+                </p>
+              </div>
+            </main>
+            <div className={css.buttonContent}>
+              <button onClick={handleAddress}>change</button>
             </div>
-            <div className={`${css.addresscontent}`}>
-              <h5 className="">DELIVERY ADDRESS</h5>
-              <p className={css.address}>
-                <span>{selectedAddress.fullName}</span> -<span>{selectedAddress.mobile}</span> ,{selectedAddress.billing_address}  <span>pinCode:{selectedAddress.pinCode}</span>
-              </p>
-            </div>
-          </main>
-          <div className={css.buttonContent}>
-            <button onClick={handleAddress}>change</button>
           </div>
-        </div>
-      ):(null)}
+        ) : (null)}
         <div className={css.paymentSummary}>
           <div className="">
             <p>3</p>
             <p>Order summary</p>
           </div>
-          <div className={`d-flex justify-content-between px-4 mt-2 ` }>
-           <p style={{fontWeight:'600'}}>Items ({orderDetails && orderDetails.totalQuantity} )</p>
+          <div className={`d-flex justify-content-between px-4 mt-2 `}>
+            <p style={{ fontWeight: '600' }}>Items ({orderDetails && orderDetails.totalQuantity})</p>
             <Link to={'/cart/ordersummary'} className={css.linkbutton}>CHANGE</Link>
           </div>
         </div>
-        
+
         <div className={css.paymentbox}>
-              <div  style={{ background: "#EDB70B" }}>
-                <p>4</p>
-                <p className='text-white'>PAYMENT OPTION</p>
+          <div style={{ background: "#EDB70B" }}>
+            <p>4</p>
+            <p className='text-white'>PAYMENT OPTION</p>
+          </div>
+          <div className="p-3">
+            <form onSubmit={handleSubmit}>
+              <div className="form-check py-2 d-flex flex-column">
+                <div>
+                  <input
+                    type="radio"
+                    id="upi"
+                    name="paymentMethod"
+                    value="upi"
+                    onChange={handleMethodChange}
+                    className="form-check-input"
+                  />
+                  <label htmlFor="upi" className="form-check-label">
+                    <img src="https://static-assets-web.flixcart.com/fk-p-linchpin-web/batman-returns/logos/UPI.gif" alt="UPI" height="20" /> UPI<br /><small>Pay by any UPI app</small>
+                  </label>
+                </div>
+                {selectedMethod === 'upi' && <input type="text" placeholder='Enter UPI ID' style={{ width: '14rem' }} />}
               </div>
-              <div className="p-3">
-                <form onSubmit={handleSubmit}>
-                  <div className="form-check py-2">
-                    <input
-                      type="radio"
-                      id="upi"
-                      name="paymentMethod"
-                      value="upi"
-                      onChange={handleMethodChange}
-                      className="form-check-input"
-                    />
-                    <label htmlFor="upi" className="form-check-label">
-                      <img src="https://static-assets-web.flixcart.com/fk-p-linchpin-web/batman-returns/logos/UPI.gif" alt="UPI" height="20" /> UPI<br /><small>Pay by any UPI app</small>
-                    </label>
-                  </div>
 
-                  <div className="form-check py-2">
-                    <input
-                      type="radio"
-                      id="wallets"
-                      name="paymentMethod"
-                      value="wallets"
-                      onChange={handleMethodChange}
-                      className="form-check-input"
-                    />
-                    <label htmlFor="wallets" className="form-check-label">
-                      <img src="https://static-assets-web.flixcart.com/fk-p-linchpin-web/batman-returns/logos/UPI.gif" alt="Wallets" height="20" /> Wallets
-                    </label>
-                  </div>
+              <div className="form-check py-2">
+                <input
+                  type="radio"
+                  id="wallets"
+                  name="paymentMethod"
+                  value="wallets"
+                  onChange={handleMethodChange}
+                  className="form-check-input"
+                />
+                <label htmlFor="wallets" className="form-check-label">
+                  <img src="https://static-assets-web.flixcart.com/fk-p-linchpin-web/batman-returns/logos/UPI.gif" alt="Wallets" height="20" /> Wallets
+                </label>
+                {selectedMethod === 'wallets' && <input type="text" placeholder='Enter Wallet ID' style={{ width: '14rem' }} />}
+              </div>
 
-                  <div className="form-check py-2">
-                    <input
-                      type="radio"
-                      id="card"
-                      name="paymentMethod"
-                      value="card"
-                      onChange={handleMethodChange}
-                      className="form-check-input"
-                    />
-                    <label htmlFor="card" className="form-check-label">
-                      Credit / Debit / ATM Card<br /><small>Add and secure cards as per RBI guidelines</small>
-                    </label>
+              <div className="form-check py-2">
+                <input
+                  type="radio"
+                  id="card"
+                  name="paymentMethod"
+                  value="card"
+                  onChange={handleMethodChange}
+                  className="form-check-input"
+                />
+                <label htmlFor="card" className="form-check-label">
+                  Credit / Debit / ATM Card<br /><small>Add and secure cards as per RBI guidelines</small>
+                </label>
+                {selectedMethod === 'card' &&
+                  <div>
+                    <input type="text" placeholder='Card Number' style={{ width: '14rem' }} />
+                    <input type="text" placeholder='Card Expiry' style={{ width: '14rem', marginTop: '0.5rem' }} />
+                    <input type="text" placeholder='CVV' style={{ width: '14rem', marginTop: '0.5rem' }} />
                   </div>
+                }
+              </div>
 
-                  <div className="form-check py-2">
-                    <input
-                      type="radio"
-                      id="net-banking"
-                      name="paymentMethod"
-                      value="net-banking"
-                      onChange={handleMethodChange}
-                      className="form-check-input"
-                    />
-                    <label htmlFor="net-banking" className="form-check-label">
-                      Net Banking<br /><small>This instrument has low success, use UPI or cards for better experience</small>
-                    </label>
-                  </div>
+              <div className="form-check py-2">
+                <input
+                  type="radio"
+                  id="net-banking"
+                  name="paymentMethod"
+                  value="net-banking"
+                  onChange={handleMethodChange}
+                  className="form-check-input"
+                />
+                <label htmlFor="net-banking" className="form-check-label">
+                  Net Banking<br /><small>This instrument has low success, use UPI or cards for better experience</small>
+                </label>
+              </div>
 
-                  <div className="form-check py-2">
-                    <input
-                      type="radio"
-                      id="cod"
-                      name="paymentMethod"
-                      value="cod"
-                      onChange={handleMethodChange}
-                      className="form-check-input"
-                    />
-                    <label htmlFor="cod" className="form-check-label">
-                      <div>
-                        Cash on Delivery
-                        <div className="mt-2">
-                          <small className="text-warning border border-danger px-3 py-1">Due to handling costs, a nominal fee of ₹10 will be charged</small>
-                          <div className="d-flex align-items-center mt-3">
-                            <div className="input-group">
-                              <span className="input-group-text text-success" id="captcha-image">
-                                {captcha}
-                                <i className="fa-solid fa-arrows-rotate text-info px-2" onClick={generateCaptcha}></i>
-                              </span>
-                              <input
-                                type="text"
-                                className="form-control px-2 py-2"
-                                id="captcha"
-                                value={input}
-                                style={{ height: "38px" }}
-                                onChange={handleInputChange}
-                                placeholder="Enter CAPTCHA"
-                              />
-                            </div>
-                          </div>
+              <div className="form-check py-2">
+                <input
+                  type="radio"
+                  id="cod"
+                  name="paymentMethod"
+                  value="cod"
+                  onChange={handleMethodChange}
+                  className="form-check-input"
+                />
+                <label htmlFor="cod" className="form-check-label">
+                  <div>
+                    Cash on Delivery
+                    <div className="mt-2">
+                      <small className="text-warning border border-danger px-3 py-1">Due to handling costs, a nominal fee of ₹10 will be charged</small>
+                      <div className="d-flex align-items-center mt-3">
+                        <div className="input-group">
+                          <span className="input-group-text text-success" id="captcha-image">
+                            {captcha}
+                            <i className="fa-solid fa-arrows-rotate text-info px-2" onClick={generateCaptcha}></i>
+                          </span>
+                          <input
+                            type="text"
+                            className="form-control px-2 py-2"
+                            id="captcha"
+                            value={input}
+                            style={{ height: "38px" }}
+                            onChange={handleInputChange}
+                            placeholder="Enter CAPTCHA"
+                          />
                         </div>
                       </div>
-                    </label>
+                    </div>
                   </div>
-
-                  <div className="form-check py-2">
-                    <input
-                      type="radio"
-                      id="emi"
-                      name="paymentMethod"
-                      value="emi"
-                      onChange={handleMethodChange}
-                      className="form-check-input"
-                    />
-                    <label htmlFor="emi" className="form-check-label">
-                      EMI (Easy Installments)<br /><small>Not applicable</small>
-                    </label>
-                  </div>
-
-                  {selectedMethod && selectedMethod !== "cod" && (
-                    <button className="btn btn-warning" type="submit" onClick={() => handleSubmit()}>
-                      Continue
-                    </button>
-                  )}
-                </form>
+                </label>
               </div>
-            </div>
 
-     
+              <div className="form-check py-2">
+                <input
+                  type="radio"
+                  id="emi"
+                  name="paymentMethod"
+                  value="emi"
+                  onChange={handleMethodChange}
+                  className="form-check-input"
+                />
+                <label htmlFor="emi" className="form-check-label">
+                  EMI (Easy Installments)<br /><small>Not applicable</small>
+                </label>
+              </div>
+
+              {selectedMethod && selectedMethod !== "cod" && (
+                <button className="btn btn-warning" type="submit">
+                  Continue
+                </button>
+              )}
+            </form>
+          </div>
+        </div>
       </section>
       <section className={css.priceDetails}>
         <div>
@@ -254,7 +266,7 @@ function Payment() {
         <main>
           <div className={css.priceTop}>
             <p>
-              <span> price (item:{orderDetails&& orderDetails.totalQuantity}) </span> <span>₹{orderDetails && orderDetails.totalPayablePrice}</span>
+              <span> price (item:{orderDetails && orderDetails.totalQuantity}) </span> <span>₹{orderDetails && orderDetails.totalPayablePrice}</span>
             </p>
             <p>
               <span>delivery charges</span>
@@ -263,28 +275,25 @@ function Payment() {
           </div>
           <div className={css.total}>
             <p>total payable</p>
-            <p>₹{orderDetails.totalPayablePrice && formatNumberWithCommas(  orderDetails.totalPayablePrice)}</p>
+            <p>₹{orderDetails.totalPayablePrice && formatNumberWithCommas(orderDetails.totalPayablePrice)}</p>
           </div>
         </main>
       </section>
       {userDetails ? (
         <>
-            {modal ? (
-                <ChangeUser user={userDetails} setModal={setModal}/>
-            )  : (
-                null
-            )}
-          { addressModal ?  (
-            <ChangeAddress userDetails={userDetails} setModal={setAddressModal} currentAdress={selectedAddress} setSelectedAddress={setSelectedAddress}/>
-          ):(null)}
+          {modal ? (
+            <ChangeUser user={userDetails} setModal={setModal} />
+          ) : (
+            null
+          )}
+          {addressModal ? (
+            <ChangeAddress userDetails={userDetails} setModal={setAddressModal} currentAdress={selectedAddress} setSelectedAddress={setSelectedAddress} />
+          ) : (null)}
         </>
-          
-      
-      ):(
+      ) : (
         null
       )}
     </section>
-    
   )
 }
 export default Payment;
--- src/service/api.js
+++ src/service/api.js
@@ -1,2 +1,3 @@
 
-export const API_URL = 'http://13.60.96.225:5000';
+export const API_URL = 'http://35.154.132.235:5000';
+// 'http://13.60.96.225:5000';
