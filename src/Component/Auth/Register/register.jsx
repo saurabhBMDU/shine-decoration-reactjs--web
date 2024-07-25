1ec1fcf30d1d918ca0a39d705e1320c94ec61e49
abaysankar <abaysankarbmdigitalutilisation@gmail.com>
2024-07-25 16:22:18 +0530

    updated package json

--- src/Component/Auth/Register/register.jsx
+++ src/Component/Auth/Register/register.jsx
@@ -14,10 +14,9 @@ function Register() {
     repeatPassword: "",
     agreeTerms: false,
   });
-
+  const [otpVerify,setOtpVerify] = useState(false)
   const [errors, setErrors] = useState({});
   const [showModal, setShowModal] = useState(false);
-
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -43,22 +42,26 @@ function Register() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
-    const formErrors = validateForm();
-    if (Object.keys(formErrors).length === 0) {
-      const removableProps = ["repeatPassword", "agreeTerms"];
-      removableProps.forEach((prop) => delete form[prop]);
-
-      dispatch(
-        doRegister(form, () => {
-          navigate("/");
-        })
-      );
+    if(!otpVerify){
+      const formErrors = validateForm();
+      if (Object.keys(formErrors).length === 0) {
+        const removableProps = ["repeatPassword", "agreeTerms"];
+        removableProps.forEach((prop) => delete form[prop]);
+        setOtpVerify(true)
+    }else {
+      setErrors(formErrors);
+      setShowModal(true);
+    }
+      
+      // dispatch(
+      //   doRegister(form, () => {
+      //     navigate("/");
+      //   })
+      // );
 
       console.log(form, "success entry");
     } else {
-      setErrors(formErrors);
-      setShowModal(true);
+    
     }
   };
 
@@ -74,86 +77,100 @@ function Register() {
       <section className="main-container">
         <form className="form" onSubmit={handleSubmit}>
           <img src="/img/shinedecoration-logo.png" alt="" style={{ objectFit: "contain", height: "3rem", margin: "0" }} />
-          <div className="title">Welcome</div>
-          <div className="subtitle">Let's create your account!</div>
-          <div className="input-container ic1">
-            <input
-              required
-              id="name"
-              className="input"
-              name="name"
-              onChange={handleInputChange}
-              value={form.name}
-              type="text"
-              placeholder=" "
-            />
-            <label htmlFor="name" className="placeholder">
-              Full name
-            </label>
-          </div>
-          <div className="input-container ic2">
-            <input
-              required
-              id="mobile"
-              className="input"
-              type="tel"
-              name="mobile"
-              placeholder=" "
-              onChange={handleInputChange}
-              value={form.mobile}
-            />
-            <label htmlFor="mobile" className="placeholder">
-              Mobile number
-            </label>
-          </div>
-          <div className="input-container ic2">
-            <input
-              required
-              id="password"
-              className="input"
-              name="password"
-              type="password"
-              onChange={handleInputChange}
-              value={form.password}
-              placeholder=" "
-            />
-            <label htmlFor="password" className="placeholder">
-              Password
-            </label>
-          </div>
-          <div className="input-container ic2">
-            <input
-              required
-              id="repeatPassword"
-              className="input"
-              name="repeatPassword"
-              type="password"
-              onChange={handleInputChange}
-              value={form.repeatPassword}
-              placeholder=" "
-            />
-            <label htmlFor="repeatPassword" className="placeholder">
-              Confirm Password
-            </label>
-          </div>
-          <div className="input-check">
-            <input
-              required
-              className="checkbox"
-              name="agreeTerms"
-              type="checkbox"
-              onChange={handleInputChange}
-              value={form.agreeTerms}
-              placeholder=" "
-            />
-            <p>
-              By continuing, you agree to Shine Decoration's <Link to="#">Terms of Service</Link> and <Link to="#">Privacy Policy.</Link>
-            </p>
-          </div>
-          <button type="submit" className="submit">
-            Submit
-          </button>
+          {!otpVerify ? (
+               <>
+               <div className="title">Welcome</div>
+               <div className="subtitle">Let's create your account!</div>
+               <div className="input-container ic1">
+                 <input
+                   required
+                   id="name"
+                   className="input"
+                   name="name"
+                   onChange={handleInputChange}
+                   value={form.name}
+                   type="text"
+                   placeholder=" "
+                 />
+                 <label htmlFor="name" className="placeholder">
+                   Full name
+                 </label>
+               </div>
+               <div className="input-container ic2">
+                 <input
+                   required
+                   id="mobile"
+                   className="input"
+                   type="tel"
+                   name="mobile"
+                   placeholder=" "
+                   onChange={handleInputChange}
+                   value={form.mobile}
+                 />
+                 <label htmlFor="mobile" className="placeholder">
+                   Mobile number
+                 </label>
+               </div>
+               <div className="input-container ic2">
+                 <input
+                   required
+                   id="password"
+                   className="input"
+                   name="password"
+                   type="password"
+                   onChange={handleInputChange}
+                   value={form.password}
+                   placeholder=" "
+                 />
+                 <label htmlFor="password" className="placeholder">
+                   Password
+                 </label>
+               </div>
+               <div className="input-container ic2">
+                 <input
+                   required
+                   id="repeatPassword"
+                   className="input"
+                   name="repeatPassword"
+                   type="password"
+                   onChange={handleInputChange}
+                   value={form.repeatPassword}
+                   placeholder=" "
+                 />
+                 <label htmlFor="repeatPassword" className="placeholder">
+                   Confirm Password
+                 </label>
+               </div>
+               <div className="input-check">
+                 <input
+                   required
+                   className="checkbox"
+                   name="agreeTerms"
+                   type="checkbox"
+                   onChange={handleInputChange}
+                   value={form.agreeTerms}
+                   placeholder=" "
+                 />
+                 <p>
+                   By continuing, you agree to Shine Decoration's <Link to="#">Terms of Service</Link> and <Link to="#">Privacy Policy.</Link>
+                 </p>
+               </div>
+               <button type="submit" className="submit">
+                 Send OTP
+               </button>
+             </>
+          )
+          :(
+            <div>
+              
+            </div>
+
+          )}
+       
         </form>
+
+            
+
         <Modal show={showModal} handleClose={handleCloseModal}>
           
         <ul className="modal-ul list-group list-group-flush ">
