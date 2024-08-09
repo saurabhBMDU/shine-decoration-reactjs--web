3f76028437a560469801c5855639de5f4cfba2b3
sainisarita <123sainisarita@gmail.com>
2024-08-09 18:47:04 +0530

    add changes

--- src/service/api.js
+++ src/service/api.js
@@ -1,6 +1,5 @@
 
-export const API_URL = `https://api.shinedecoration.com`
+// export const API_URL = `https://api.shinedecoration.com`
+export const API_URL='http://localhost:5000'
 
-// 'http://35.154.132.235:5000';
-// 'http://13.60.96.225:5000';
 
